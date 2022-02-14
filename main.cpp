bdb8cd89f96d2e43b6751d854d284031631a422b
PCristian00 <cristianp9300@gmail.com>
2022-02-14 19:03:05 +0100

    mia versione

--- main.cpp
+++ main.cpp
@@ -1,45 +1,93 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+
 using namespace std;
 #define CARATTERI_MAX 10
-struct nazione
-{
+struct nazione {
     char nome[CARATTERI_MAX + 1];
     int ori;
     int argenti;
     int bronzi;
 };
+
 /* Restituisce il numero di righe contenute nel file “nomefile”. Si tenga
 presente che ogni riga è strutturata come nell'esempio “medagliere.txt”. */
-int contaRighe(const char* nomefile)
-{
-// 4 Punti
+int contaRighe(const char *nomefile) {
+    fstream in;
+    in.open(nomefile, ios::in); // ERRORE: messo solo un set di punti e virgola
+    int c = 0;
+    while (in.good()) c++;
+    in.close();
+    return c;
+// 3/4 Punti
 }
+
 /* Legge in input il file “nomefile” e popola il vettore “nazionali”, con il
 nome, gli ori, gli argenti e i bronzi di ogni nazione. */
-void leggiFile(nazione nazionali[], const char* nomefile)
-{
-// 4 Punti
+void leggiFile(nazione nazionali[], const char *nomefile) {
+    fstream in;
+    in.open(nomefile, ios::in); // ERRORE: messo solo un set di punti e virgola
+    int i = 0;
+    while (in.good()) {
+        in >> nazionali[i].nome >> nazionali[i].ori >> nazionali[i].argenti >> nazionali[i].bronzi;
+        i++;
+    }
+    in.close();
+// 3/4 Punti
 }
+
 /* Ordina il vettore “nazionali” di lunghezza “n” in maniera decrescente
 secondo i seguenti criteri:
 – numero di ori
 – a parità di ori, numero di argenti
 – a parità di ori e argenti, numero di bronzi. */
-void ordinaNazioni(nazione nazionali[], int n)
-{
-// 4 Punti
+void ordinaNazioni(nazione nazionali[], int n) {
+    //ERRORE abb. grave: ho scritto n[] anziche nazionali[] ad ogni cosa
+    nazione tmp{};
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (nazionali[j].ori > nazionali[i].ori) {
+                tmp = nazionali[i];
+                nazionali[i] = nazionali[j];
+                nazionali[j] = tmp;
+            } else if (nazionali[j].ori == nazionali[i].ori)
+                if (nazionali[j].argenti > nazionali[j].argenti) {
+                    tmp = nazionali[i];
+                    nazionali[i] = nazionali[j];
+                    nazionali[j] = tmp;
+                } else if (nazionali[j].argenti == nazionali[i].argenti)
+                    if (nazionali[j].bronzi > nazionali[j].bronzi) {
+                        tmp = nazionali[i];
+                        nazionali[i] = nazionali[j];
+                        nazionali[j] = tmp;
+                    }
+
+// 2/4 Punti
 }
+
 /* Stampa a schermo il vettore “nazionali” di lunghezza n, come nell'esempio
 di esecuzione. Ogni colonna ha l'intestazione “Ori”, “Argenti”, “Bronzi”,
 come nell'esempio di esecuzione. */
-void stampaMedagliere(nazione nazionali[], int n)
-{
+void stampaMedagliere(nazione nazionali[], int n) {
+    cout << "\t \t Ori \t Argenti";
+    cout << "\t Bronzi" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << nazionali[i].ori << "\t" << nazionali[i].argenti << "\t";
+        cout << nazionali[i].bronzi << endl;
+    }
 // 3 Punti
 }
+
 // Programma principale
-int main()
-{
+int main() {
+    int size = contaRighe("C:\\Users\\Thinkpad User\\CLionProjects\\medaglie\\medagliere.txt");
+    cout<<size;
+    nazione nazionali[size];
+    leggiFile(nazionali, "C:\\Users\\Thinkpad User\\CLionProjects\\medaglie\\medagliere.txt");
+    ordinaNazioni(nazionali, size);
+    stampaMedagliere(nazionali, size);
+    return 0;
 // 3 Punti
 }
